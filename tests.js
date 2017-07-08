6aff4eba1fba77753ca0ba9a7afd36e29d6c4f1a
Luke <lrossy@gmail.com>
2017-07-08 05:21:43 -0400

    Answers

--- tests.js
+++ tests.js
@@ -1,18 +1,21 @@
-var ws = require('./workshop.js');
+var ws = require('./workshop');
 
-// console.log('getIssPosition: ', ws.getIssPosition());
+// // ISS open-notify.org
+ws.getIssPosition()
+    .then(console.log, console.error);
 
-// ws.getIssPosition().then(function(resolve){
-//     console.log('resolve', resolve);
-// })
-//     .catch( (e) =>{
-//         console.log('some err:', e)
-//     });
+// darksky
+ws.getCurrentTemperatureAtPosition({lat: 37.8267, lng: -122.4233})
+    .then(console.log, console.error);
 
-ws.getAddressPosition()
-    .then(function(resolve){
-        console.log('resolve', resolve);
-    })
-    .catch( (e) =>{
-        console.log('some err:', e)
-    });
+// gmaps
+ws.getAddressPosition('3 Place Ville Marie #401, Montreal, QC H3B 2E3')
+    .then(console.log, console.error);
+
+// gmaps then darksky
+ws.getCurrentTemperature('3 Place Ville Marie #401, Montreal, QC H3B 2E3')
+    .then(console.log, console.error);
+
+ws.getDistanceFromIss('3 Place Ville Marie #401, Montreal, QC H3B 2E3')
+    .then(console.log, console.error);
+
--- workshop.js
+++ workshop.js
@@ -6,73 +6,112 @@ function getDistance(pos1, pos2) {
 }
 
 function getIssPosition() {
-    var options = {
-        uri: 'http://api.open-notify.org/iss-now.json',
-        headers: {
-            'User-Agent': 'Request-Promise'
-        },
-        json: true // Automatically parses the JSON string in the response
+
+    var location = {
+        url: 'http://api.open-notify.org/iss-now.json',
+        json: true
     };
-    return request(options)
-    .then(
-        function(response) {
-            // throw new Error('test error')
-          // Parse as JSON
-          // Return object with lat and lng
-          if(response.message !== "success"){
-            return {};
-          }
-          else{
-            return {
-              lat: response.iss_position.latitude,
-              lng: response.iss_position.longitude
+
+    var cbFn = function(response) {
+        var formattedResponse = {};
+        if(response && response.message === "success"){
+            formattedResponse = {
+                name: 'getIssPosition',
+                lat: response.iss_position.latitude,
+                lng: response.iss_position.longitude
             };
-          }
         }
-    )
+        else{
+            formattedResponse.message = response.message;
+        }
+        return formattedResponse;
+    };
+
+    return request(location)
+        .then(cbFn);
+        // .catch( (error) => error.error );
 }
 
 function getAddressPosition(address) {
 
+    if(!address) return Promise.reject('Address required');
+
     const API_KEY = 'AIzaSyBXJdny-NywdTQk7UJQNI_BZIApH_MIsaU';
     var options = {
-        uri: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=' + API_KEY,
+        uri: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + 'key=' + API_KEY,
         headers: {
             'User-Agent': 'Request-Promise'
         },
         json: true // Automatically parses the JSON string in the response
     };
+
+    var cbFn = function(response) {
+        if(response.status !== "OK" || response.results === undefined || response.results.length < 1 ){
+            return {};
+        }
+        else{
+            return {
+                name: 'getAddressPosition',
+                lat: response.results[0].geometry.location.lat,
+                lng: response.results[0].geometry.location.lng
+            };
+        }
+    };
+
     return request(options)
-        .then(
-            function(response) {
-                // throw new Error('test error')
-                // Parse as JSON
-                // Return object with lat and lng
-                if(response.status !== "OK" || typeof response.results === undefined || response.results.length < 1 ){
-                    return {};
-                }
-                else{
-                    return {
-                        lat: response.results[0].geometry.location.lat,
-                        lng: response.results[0].geometry.location.lng
-                    };
-                }
-            }
-        )
+        .then(cbFn);
 }
 
 function getCurrentTemperatureAtPosition(position) {
-    var API_KEY = 'c861dc54a43dcc5756a3b065d1c354e7';
+    const API_KEY = 'c861dc54a43dcc5756a3b065d1c354e7';
+    if(!position) return Promise.reject('lat and lng required: getCurrentTemperatureAtPosition({lat: 123, lng: 321})');
 
+    var options = {
+        uri: 'https://api.darksky.net/forecast/' + API_KEY + '/' + position.lat + ',' + position.lng,
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true // Automatically parses the JSON string in the response
+    };
 
+    var cbFn = function(response, reject) {
+        if(reject){
+            console.error('reject',reject)
+        }
+        return {
+            name: 'getCurrentTemperatureAtPosition',
+            temperature: response.currently.temperature
+        };
+    };
+
+    return request(options)
+        .then(cbFn);
 }
 
 function getCurrentTemperature(address) {
 
+    return getAddressPosition(address)
+        .then(getCurrentTemperatureAtPosition)
+        .then( result => {
+                result.name = 'getCurrentTemperature';
+                return result;
+        });
 }
 
 function getDistanceFromIss(address) {
 
+    var promises = [
+        getAddressPosition(address),
+        getIssPosition()
+    ];
+
+    return Promise.all(promises)
+        .then( values => {
+            return {
+                name: 'getDistanceFromIss',
+                distance:  getDistance(values[0], values[1])
+            };
+        });
 }
 
 module.exports = {
